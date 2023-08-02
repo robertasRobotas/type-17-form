723fc9e2539c66929c18fce55ab55d833f8371b2
Robertas <r.ankudovicius@gmail.com>
2023-08-02 10:16:45 +0300

    Added error and success messaged to the screen

--- script.js
+++ script.js
@@ -6,27 +6,49 @@ const onButtonClick = ()=>{
    const email = document.getElementById('email').value;
    const password = document.getElementById('password').value;
 
-    if(name && age && email && password){
-        
-        if(password.length < 8){
-            console.log('Password should contain 8 letters');
-        }
-        
-        if(Number(age) < 18){
-            console.log('You have to be at least 18 years old');
-        }
-        
-        if(!email.includes('@')){
-            console.log('Email should contain @ sign');
-        }
-        
-        if(email.includes('@') && (Number(age) >= 18) && password.length >= 8){
-            console.log(name, age, email, password);
-        }
+   const errorWrapper = document.getElementById("error-wrapper");
+   errorWrapper.innerHTML = "";
 
-    }else{
-        console.log("Please fill all the imputs");
-    }
+   if (name && age && email && password) {
+     if (!email.includes("@")) {
+       const errorWrapper = document.getElementById("error-wrapper");
+       const error = document.createElement("div");
+       error.innerHTML = "Email should contain @ sign";
+       errorWrapper.append(error);
+     }
+
+     // if (!Number(age) || Number(age) < 18) {
+     if (Number(age) < 18) {
+       const errorWrapper = document.getElementById("error-wrapper");
+       const error = document.createElement("div");
+       error.innerHTML = "You have to be at least 18 years old";
+       errorWrapper.append(error);
+     }
+
+     if (!Number(age)) {
+       const errorWrapper = document.getElementById("error-wrapper");
+       const error = document.createElement("div");
+       error.innerHTML = "Please type a number inside age input";
+       errorWrapper.append(error);
+     }
+
+     if (password.length < 8) {
+       const errorWrapper = document.getElementById("error-wrapper");
+       const error = document.createElement("div");
+       error.innerHTML = "Password should contain 8 letters";
+       errorWrapper.append(error);
+     }
+
+     if (email.includes("@") && Number(age) >= 18 && password.length >= 8) {
+       console.log(name, age, email, password);
+
+       const successWrapper = document.getElementById("success-wrapper");
+       successWrapper.innerHTML = "Validation successfull";
+     }
+   } else {
+     const errorWrapper = document.getElementById("error-wrapper");
+     errorWrapper.innerHTML = "Please fill all the inputs";
+   }
    
 }
 
